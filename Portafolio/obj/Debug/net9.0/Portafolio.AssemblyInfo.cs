e4849014156e98f1ab02ef05ae98df484ead4476
matiaschedas <matias.chedas@hotmail.com>
2025-07-05 01:27:05 -0300

    avance

--- Portafolio/obj/Debug/net9.0/Portafolio.AssemblyInfo.cs
+++ Portafolio/obj/Debug/net9.0/Portafolio.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portafolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c5d2d5274c88aaaca33561cd187f76b4fccd5a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6b3013758776c1e279d19da371773010e86b81b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portafolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portafolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
